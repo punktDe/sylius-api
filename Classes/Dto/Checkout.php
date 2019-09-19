f05ebf430771d727b4f6ff9c6e6793943d319a29
Paul Weiske <weiske@punkt.de>
2019-09-19 15:43:51 +0200

    FEATURE: Enable cart checkout

---
+++ Classes/Dto/Checkout.php
@@ -0,0 +1,177 @@
+<?php
+declare(strict_types=1);
+
+namespace PunktDe\Sylius\Api\Dto;
+
+/*
+ *  (c) 2019 punkt.de GmbH - Karlsruhe, Germany - http://punkt.de
+ *  All rights reserved.
+ */
+
+class Checkout implements ApiDtoInterface
+{
+    /**
+     * @var string
+     */
+    protected $identifier;
+
+    /**
+     * @var array
+     */
+    protected $shippingAddress = [
+        'firstName' => '',
+        'lastName' => '',
+        'street' => '',
+        'countryCode' => '',
+        'city' => '',
+        'postcode' => '',
+    ];
+
+    /**
+     * @var bool
+     */
+    protected $differentBillingAddress = false;
+
+    /**
+     * @var array
+     */
+    protected $shipments = [
+        'method' => 'no-shipment'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $payments = [
+            'method' => 'bill'
+    ];
+
+    /**
+     * @var string
+     */
+    protected $notes = '';
+
+
+    /**
+     * @param string $identifier
+     * @return Checkout
+     */
+    public function setCartIdentifier(string $identifier): Checkout
+    {
+        $this->identifier = $identifier;
+        return $this;
+    }
+
+    /**
+     * @param string $firstName
+     * @param string $lastName
+     * @param string $city
+     * @param string $postcode
+     * @param string $street
+     * @param string $country
+     * @return Checkout
+     */
+    public function setShippingAddress(string $firstName, string $lastName, string $city, string $postcode, string $street, string $country): Checkout
+    {
+        $this->shippingAddress = [
+            'firstName' => $firstName,
+            'lastName' => $lastName,
+            'street' => $street,
+            'countryCode' => $country,
+            'city' => $city,
+            'postcode' => $postcode,
+        ];
+        return $this;
+    }
+
+
+    /**
+     * @param string $shipmentMethod
+     */
+    public function setShipmentMethod(string $shipmentMethod)
+    {
+        $this->shipments = [
+            'method' => $shipmentMethod
+        ];
+    }
+
+    /**
+     * @param string $paymentMethod
+     */
+    public function setPaymentMethod(string $paymentMethod)
+    {
+        $this->payments = [
+            'method' => $paymentMethod
+        ];
+    }
+
+    /**
+     * @param string $euustid
+     * @param string $pubkey
+     * @param string $comment
+     * @return Checkout
+     */
+    public function setNotes(string $euustid, string $pubkey, string $comment): Checkout
+    {
+        $this->notes = "Die EU-UstID lautet ${euustid}.\n Der Public Key lautet: \n\n ${pubkey} \n\n. \n Zur BEstellung wurde folgender Kommentar abgegeben: \n ${comment} \n.";
+        return $this;
+    }
+
+    /**
+     * Returns the cart identifier used for put operations
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * @return array
+     */
+    public function getShippingAddress(): array
+    {
+        return $this->shippingAddress;
+    }
+
+    /**
+     * @return array
+     */
+    public function getShippingAddressData(): array
+    {
+        return [
+            'shippingAddress' => $this->getShippingAddress(),
+            'differentBillingAddress' => false
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getShippingMethodData(): array
+    {
+        return [
+            'shipments' => array($this->shipments)
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getPaymentData(): array
+    {
+        return [
+            'payments' => array($this->payments)
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getNoteData(): array
+    {
+        return [
+            'notes' => $this->notes
+        ];
+    }
+}
